6b8b521af87028348d30fc0126e6b047cc505176
Evgeniy Poliezhaiev <evgeniyy2011@gmail.com>
2026-05-01 20:17:49 +0100

    3.2 home work

--- Home work 3.2.py
+++ Home work 3.2.py
@@ -1,7 +1,10 @@
 # ДЗ 3.2. Перемістити елемент у списку
 from operator import index
 
-lst_one = []
+lst_one = [12, 3, 4, 10]
+# lst_one = [1]
+# lst_one = []
+# lst_one = [12, 3, 4, 10, 8]
 new_lst = []
 if len(lst_one) >= 1:
     new_lst.append(lst_one[-1])

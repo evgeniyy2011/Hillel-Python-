c2f30ce08dcf53a8cf0fede38df2f145ee0c2e8b
Evgeniy Poliezhaiev <evgeniyy2011@gmail.com>
2026-05-09 23:52:50 +0100

    5.3 home work

--- Home work 5.3.py
+++ Home work 5.3.py
@@ -1,2 +1,14 @@
 # ДЗ 5.3. hashtag
-vvod = input("Enter string")
+
+sentence = input("Enter string")
+result = []
+new = []
+for i in sentence.split(" "):
+    result.append(i.title())
+result = list("".join(result))
+new.append("#")
+for q in result:
+    if q.isalnum():
+        new.append(q)
+new = "".join(new)
+print(new[:140])

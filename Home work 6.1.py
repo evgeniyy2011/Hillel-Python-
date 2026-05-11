e4a72592912e57a3fdfa3f13c32f13358da08269
Evgeniy Poliezhaiev <evgeniyy2011@gmail.com>
2026-05-11 21:53:35 +0100

    6.1 home work

---
+++ Home work 6.1.py
@@ -0,0 +1,9 @@
+import string
+
+new_string = input("Введите две буквы латинского алфавита через дефис (Наример: a-z) ")
+first = string.ascii_letters.index(new_string[0])
+second = string.ascii_letters.index(new_string[2])
+result = ""
+for i in range(first, second + 1):
+    result += string.ascii_letters[i]
+print(result)

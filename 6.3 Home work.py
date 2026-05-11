71ba7bcd7b877822d1b38923b727d847459358d8
Evgeniy Poliezhaiev <evgeniyy2011@gmail.com>
2026-05-11 23:09:57 +0100

    6.3 home work

---
+++ 6.3 Home work.py
@@ -0,0 +1,8 @@
+# ДЗ 6.3. Добуток чисел
+
+numb = list(input("Enter your number - "))
+new_numb = "*".join(numb)
+result = 0
+while eval(new_numb) >= 9:
+    new_numb = "*".join(str(eval(new_numb)))
+print(eval(new_numb))

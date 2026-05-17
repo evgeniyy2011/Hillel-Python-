d0043a793decca5124ef3ac5b3a461b686bdec32
Evgeniy Poliezhaiev <evgeniyy2011@gmail.com>
2026-05-17 21:41:16 +0100

    14.05.2026 extra home work

--- Home work 7.4.py
+++ Home work 7.4.py
@@ -1,16 +1,16 @@
 # ДЗ 7.4. Пошук спільних елементів
 
 
-def common_elements():
-    first_set = set()
-    second_set = set()
-    for q in range(100):
-        if q % 3 == 0:
-            first_set.add(q)
-        if q % 5 == 0:
-            second_set.add(q)
-    return first_set.intersection(second_set)
-
-
-assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
-print(common_elements())
+# def common_elements():
+#     first_set = set()
+#     second_set = set()
+#     for q in range(100):
+#         if q % 3 == 0:
+#             first_set.add(q)
+#         if q % 5 == 0:
+#             second_set.add(q)
+#     return first_set.intersection(second_set)
+#
+#
+# assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
+# print(common_elements())

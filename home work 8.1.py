cf9bbf84fab9b2a832a8b37b54221de94ccc352a
Evgeniy Poliezhaiev <evgeniyy2011@gmail.com>
2026-05-19 22:31:31 +0100

    Home work 8.1

---
+++ home work 8.1.py
@@ -0,0 +1,14 @@
+def add_one(some_list):
+    new = ""
+    for w in some_list:
+        new += str(w)
+    new = int(new) + 1
+    result = [int(i) for i in str(new)]
+    return result
+
+
+# assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], "Test1"
+# assert add_one([9, 9, 9]) == [1, 0, 0, 0], "Test2"
+# assert add_one([0]) == [1], "Test3"
+assert add_one([9]) == [1, 0], "Test4"
+print("ОК")

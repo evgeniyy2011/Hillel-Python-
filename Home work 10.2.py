667202b1ef1e977fb3a1d782d5ffd826b244267f
Evgeniy Poliezhaiev <evgeniyy2011@gmail.com>
2026-05-26 20:53:04 +0100

    Home work 10.3

--- Home work 10.2.py
+++ Home work 10.2.py
@@ -9,10 +9,12 @@ def first_word(text):
     return revo
 
 
+print(first_word("greetings, friends"))
+
 # assert first_word("Hello world") == "Hello", "Test1"
 # assert first_word("greetings, friends") == "greetings", "Test2"
 # assert first_word("don't touch it") == "don't", "Test3"
 # assert first_word(".., and so on ...") == "and", "Test4"
 # assert first_word("hi") == "hi", "Test5"
-assert first_word("Hello.World") == "Hello", "Test6"
+# assert first_word("Hello.World") == "Hello", "Test6"
 print("OK")

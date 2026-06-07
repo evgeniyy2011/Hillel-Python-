b95f4c72b41961850d19c326c4b44e6ff1a83229
Evgeniy Poliezhaiev <evgeniyy2011@gmail.com>
2026-06-07 20:23:45 +0100

    Home work 13.2

---
+++ Home work 13.2 extra.py
@@ -0,0 +1,9 @@
+class Item:
+    """'Класс товару"""
+
+    def __init__(self, price, describe, dimension):
+        self.price = price
+        self.describe = describe
+        self.dimension = dimension
+
+    towar_1 = Item(
